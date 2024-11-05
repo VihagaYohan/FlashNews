63a0516c378d0b57fec31b70f2f262afc3fc1e9b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-05 18:33:04 +0530

    Code refactoring

--- app/src/main/java/com/techtribeservices/flashnews/presentation/MainActivity.kt
+++ app/src/main/java/com/techtribeservices/flashnews/presentation/MainActivity.kt
@@ -2,51 +2,61 @@ package com.techtribeservices.flashnews.presentation
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techtribeservices.flashnews.BuildConfig
+import com.techtribeservices.flashnews.data.web.NewsApi
+import com.techtribeservices.flashnews.presentation.home.HomeScreen
 import com.techtribeservices.flashnews.ui.theme.FlashNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var newsApi: NewsApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        GlobalScope.launch {
+            var response = newsApi.getEverything()
+            Log.d("Flash", response.body().toString())
+        }
+
         enableEdgeToEdge()
         setContent {
             FlashNewsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    ) {
+                        HomeScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Text(
-        text = "Hello $name! - ${BuildConfig.API_KEY}",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FlashNewsTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/techtribeservices/flashnews/presentation/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.techtribeservices.flashnews.presentation.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
+
