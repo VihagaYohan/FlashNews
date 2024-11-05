3fad82c49d857461d8a2daaac960c6ddc3a69f5b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-05 22:38:04 +0530

    Added - Basic viewModel implementation along with UI components change after view change

--- app/src/main/java/com/techtribeservices/flashnews/presentation/common/State.kt
+++ app/src/main/java/com/techtribeservices/flashnews/presentation/common/State.kt
@@ -2,6 +2,6 @@ package com.techtribeservices.flashnews.presentation.common
 
 sealed class State<out T> {
     object Loading: State<Nothing>()
-    class Success<T>(data: T): State<T>()
+    class Success<T>(val data: T): State<T>()
     class Error(val message: String): State<Nothing>()
 }
--- app/src/main/java/com/techtribeservices/flashnews/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/techtribeservices/flashnews/presentation/home/HomeScreen.kt
@@ -1,14 +1,20 @@
 package com.techtribeservices.flashnews.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.techtribeservices.flashnews.data.response.NewsListResponse
+import com.techtribeservices.flashnews.presentation.common.State
+import com.techtribeservices.flashnews.presentation.components.ErrorMessage
+import com.techtribeservices.flashnews.presentation.components.LoadingIndicator
 import com.techtribeservices.flashnews.presentation.viewModel.HomeViewModel
 
 @Composable
@@ -16,6 +22,32 @@ fun HomeScreen() {
     val viewModel: HomeViewModel = hiltViewModel()
     val uiState = viewModel.state.collectAsState()
 
-
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            when(uiState.value) {
+                State.Loading -> LoadingIndicator()
+                is State.Error -> ErrorMessage()
+                is State.Success -> NewsList(uiState.value)
+            }
+        }
+    }
 }
+
+@Composable
+fun NewsList(newsList: State<NewsListResponse>) {
+    val data = (newsList as State.Success).data
+
+    Text(
+        text = "News list goes here ${data.articles?.size}"
+    )
+}
+
+
 
